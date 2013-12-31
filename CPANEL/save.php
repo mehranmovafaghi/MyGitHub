243104a0d33a30b8c5b174a6ad82d784811417c3
Mehran Movafaghi <mehranmovafaghi@yahoo.com>
2013-12-31 04:46:54 -0600

    CPANEL
    CPANEL

---
+++ CPANEL/save.php
@@ -0,0 +1,12 @@
+﻿<?php
+$myfile = "Testfile.txt";
+$fh = fopen($myfile, 'a') or die("CANNOT OPEN FILE");
+$fname = $_POST["fname"];
+$Lname = $_POST["Lname"];
+$phnum = $_POST["phnum"];
+$DATA = $fname . " " . $Lname . " " . "$phnum" . "\n";
+fwrite($fh, $DATA);
+fclose($fh);
+header("Location: http://192.168.1.83/index.html");
+?>
+
