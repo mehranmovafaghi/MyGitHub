cb29dfd4a92c49c219a86bbf48b709eb0ae4ef18
Mehran Movafaghi <mehranmovafaghi@yahoo.com>
2014-01-05 22:25:01 -0600

    JavaScript
    JavaScript

---
+++ JavaScript/CPANEL/loadmenu.js
@@ -0,0 +1,15 @@
+﻿function loadXMLDoc_2(dname) {
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    }
+    else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.open("GET", dname, false);
+    xhttp.send();
+    return xhttp.responseXML;
+}
+xmlDoc = loadXMLDoc_2("menu.xml");
+
+w = xmlDoc.getElementsByTagName("title");
+z = xmlDoc.getElementsByTagName("href");
---
+++ JavaScript/CPANEL/loadxml.js
@@ -0,0 +1,24 @@
+﻿function loadXMLDoc(dname) {
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    }
+    else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.open("GET", dname, false);
+    xhttp.send();
+    return xhttp.responseXML;
+}
+xmlDoc = loadXMLDoc("context.xml");
+$article_title = xmlDoc.getElementsByTagName("article_title");
+$article_content = xmlDoc.getElementsByTagName("article_content");
+//w = xmlDoc.getElementsByTagName("article_author");
+//z = xmlDoc.getElementsByTagName("article_date");
+
+function mycontent() {
+    for (i = 0; i < $article_title.length; i++) {
+        document.write("<div class='container'><div class='titlebox'><h2>" + $article_title[i].childNodes[0].nodeValue + "</h2>" + "</div>");
+        document.write("<div class='contentbox'><p>" + $article_content[i].childNodes[0].nodeValue + "</p></div></div>");
+    }
+}
+mycontent();
